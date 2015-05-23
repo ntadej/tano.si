2b9bc7ae677b0a9fad035dff467950484076be3a
Tadej Novak <tadej@tano.si>
2015-05-23 11:03:13 +0200

    Webpage migration to Jekyll + jekyll-assets + Skeleton (part 2)

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,4 +1,5 @@
 //= require browserdetect
+//= require download
 //= require cookies
 
 $(document).ready(function()
@@ -8,10 +9,29 @@ $(document).ready(function()
         win = $(window),
         doc = $(document);
 
+    function sidebarResize() {
+        if ($(window).width() < 768) {
+            $('.sidebar-offcanvas').addClass('anim');
+        } else {
+            $('.sidebar-offcanvas').removeClass('anim');
+            $('.row-offcanvas').removeClass('active');
+        }
+    };
+
     function init()
     {
         $("a[href^='#']").smoothScroll();    
         //cookies.showMessageLoad();
+
+        $('[data-toggle=offcanvas]').click(function() {
+            $('.row-offcanvas').toggleClass('active');
+        });
+
+        sidebarResize();
+        $(window).resize(sidebarResize);
+
+        BrowserDownload.Tano();
+        BrowserTabs.Tano();
     }
 
     init();
