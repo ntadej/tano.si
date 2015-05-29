2fe897b60fbc97339149ec0acd7860132cf7547b
Tadej Novak <tadej@tano.si>
2015-05-29 19:54:33 +0200

    Add forum images

---
+++ src/forum/gen.sh
@@ -0,0 +1,3 @@
+inkscape --export-png ../../_assets/images/forum/logo-small.png -a 80:80:520:520 -w 80 ../favicon/favicon.svg
+
+inkscape --export-png ../../_assets/images/forum/logo.png -h 80 forum.svg
