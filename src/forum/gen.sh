80c98c702b3fbbe9b66d4bc7992276fca1d1a883
Tadej Novak <tadej@tano.si>
2015-05-29 20:52:39 +0200

    Forum system avatar

--- src/forum/gen.sh
+++ src/forum/gen.sh
@@ -1,3 +1,4 @@
 inkscape --export-png ../../_assets/images/forum/logo-small.png -a 80:80:520:520 -w 80 ../favicon/favicon.svg
+inkscape --export-png ../../_assets/images/forum/avatar.png -a 80:80:520:520 -w 240 ../favicon/favicon.svg
 
 inkscape --export-png ../../_assets/images/forum/logo.png -h 80 forum.svg
