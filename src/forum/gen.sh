db2fe043d55589bc5e8de92f0efcaa95c6040294
Tadej Novak <tadej@tano.si>
2015-05-30 18:40:41 +0200

    Google OAuth icon

--- src/forum/gen.sh
+++ src/forum/gen.sh
@@ -1,4 +1,5 @@
 inkscape --export-png ../../_assets/images/forum/logo-small.png -a 80:80:520:520 -w 80 ../favicon/favicon.svg
 inkscape --export-png ../../_assets/images/forum/avatar.png -a 80:80:520:520 -w 240 ../favicon/favicon.svg
+inkscape --export-png ../../_assets/images/forum/google.png -a 80:80:520:520 -w 120 ../favicon/favicon.svg
 
 inkscape --export-png ../../_assets/images/forum/logo.png -h 80 forum.svg
