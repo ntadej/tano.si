1331fba6593a2893954e77fc0c19e095e9f91534
Tadej Novak <tadej@tano.si>
2015-09-12 21:57:15 +0200

    Update build files and enable CDN

---
+++ Rakefile
@@ -0,0 +1,28 @@
+require 'fileutils'
+
+@output = '/var/www/default/'
+@cdn = '/var/www/cdn/'
+@cdn_name = 'tano'
+
+task default: %w[serve]
+
+task :serve do
+  puts `bundle exec jekyll serve --watch`
+end
+
+task :deploy do
+  puts 'Building web page using Jekyll'
+
+  puts `bundle exec jekyll build --config _config.yml,_config_deploy.yml --destination #{@output}`
+
+  puts ''
+  puts 'Copying assets and other resources to CDN'
+  FileUtils.cp_r "#{@output}error/.", "#{@cdn}error", :verbose => true
+
+  FileUtils.rm_rf("#{@cdn}#{@cdn_name}/")
+
+  FileUtils.mkdir("#{@cdn}#{@cdn_name}")
+  FileUtils.cp_r "#{@output}assets/", "#{@cdn}#{@cdn_name}/assets", :verbose => true
+  FileUtils.cp_r "#{@output}files/", "#{@cdn}#{@cdn_name}/files", :verbose => true
+  FileUtils.cp_r "#{@output}resources/", "#{@cdn}#{@cdn_name}/resources", :verbose => true
+end
--- _plugins/image.rb
+++ _plugins/image.rb
@@ -1,17 +1,22 @@
 module Jekyll
-  	class ImageTag < Liquid::Tag
+    class ImageTag < Liquid::Tag
 
-    	def initialize(tag_name, params, tokens)
-      		super
-      		@url = params.split('|')[0]
-      		@text = params.split('|')[1]
-      		@align = params.split('|')[2]
-    	end
+      def initialize(tag_name, params, tokens)
+          super
+          @url = params.split('|')[0]
+          @text = params.split('|')[1]
+          @align = params.split('|')[2]
+      end
 
-    	def render(context)
-      		"<div class=\"thumbnail thumb-#{@align}\"><img src=\"#{@url}\" alt=\"#{@text}\"><div class=\"caption\">#{@text}</div></div>"
-    	end
-  	end
+      def render(context)
+          prefix = ''
+          if context.registers[:site].config['production']
+            prefix = context.registers[:site].config['cdn_url']
+          end
+
+          "<div class=\"thumbnail thumb-#{@align}\"><img src=\"#{prefix}#{@url}\" alt=\"#{@text}\"><div class=\"caption\">#{@text}</div></div>"
+      end
+    end
 end
 
 Liquid::Template.register_tag('img', Jekyll::ImageTag)
